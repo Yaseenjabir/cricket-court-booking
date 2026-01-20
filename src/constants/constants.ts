9535317af727be406f16afb1f246170c6e9d9690
waqas2601 <mw2601005@gmail.com>
2026-01-21 01:08:31 +0500

    Admin promos API integration

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,5 +6,7 @@ export const COURTS_URL = "/courts";
 export const UPDATE_COURT_URL = (id: string) => `/courts/${id}`;
 export const DELETE_COURT_URL = (id: string) => `/courts/${id}`;
 
-// Promos
+// Promo Codes URLs
 export const PROMO_CODES_URL = "/promo-codes";
+export const UPDATE_PROMO_CODE_URL = (id: string) => `/promo-codes/${id}`;
+export const DELETE_PROMO_CODE_URL = (id: string) => `/promo-codes/${id}`;
